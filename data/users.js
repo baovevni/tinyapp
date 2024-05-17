f89b302f4ef3a7801a472c6229755e5fbd0fec83
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-05-16 21:30:09 -0400

    Update users.js

--- data/users.js
+++ data/users.js
@@ -1,7 +1,7 @@
 const users = {
   abc1A: {
     id: "abc1A",
-    email: "user@example.com",// ebarducov@gmail.com
+    email: "user@example.com",
     password: "$2a$10$yjDx5ZSEC4GJbNQItinzDeTnu6RIrn8FAPKNH/Lx8wuYsv9jtXvEy",
   },
   def2B: {
